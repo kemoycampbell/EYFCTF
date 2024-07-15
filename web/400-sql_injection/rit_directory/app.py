bb4b58b32948ca546504c40c88e826e5d93cbdd6
kemoycampbell <ksc2650@rit.edu>
2024-07-15 08:22:29 -0400

    update some bug fixes

--- web/400-sql_injection/rit_directory/app.py
+++ web/400-sql_injection/rit_directory/app.py
@@ -17,6 +17,7 @@ app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST", "localhost")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_PORT'] = 3307
 
 
 from flask import session
