a25bcbe7fccab7c498f4aaa48c184180bbf7a0f8
Michael Berrios <mxbics@rit.edu>
2025-06-02 18:30:17 -0400

    Hide challenges 300 to 500, show only 100 and 200

--- web/300-password_recovery/index.php
+++ web/300-password_recovery/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
     header('Location: login.html');
     exit;
 }
